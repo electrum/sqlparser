00ee6815444384c5d2140033c8f12b4f25641cb7
David Phillips <david@acz.org>
2008-03-16 23:21:37 -0500

    Added DDL to test script

--- test.sh
+++ test.sh
@@ -2,6 +2,15 @@
 
 test -f Test.class || exit 100
 
+dotest()
+{
+   CLASSPATH=$CLASSPATH:antlr java Test
+   if [ $? -eq 0 ] ; then echo "passed"; else echo "failed"; fi
+}
+
+echo "*** testing ddl ***"
+cat ~/tpch/dss.ddl | dotest
+
 for n in $(seq 1 22)
 do
    test $n -eq 15 && continue
@@ -12,6 +21,5 @@ do
    sed 's/^:o$//' | \
    sed 's/^:n -\?[0-9]\+$//' | \
    sed "s/\([^']\):\([0-9]\+\)/\\1\\2/g" | \
-   CLASSPATH=$CLASSPATH:antlr java Test
-   if [ $? -eq 0 ] ; then echo "passed"; else echo "failed"; fi
+   dotest
 done
