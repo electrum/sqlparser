ab942b2ac11ed3d1dfb8c33555521a97405cb903
David Phillips <david@acz.org>
2010-03-18 11:00:15 -0700

    Fix for Mac

--- test.sh
+++ test.sh
@@ -1,25 +1,24 @@
-#!/bin/sh
+#!/bin/bash
 
 test -f Test.class || exit 100
 
 dotest()
 {
-   CLASSPATH=$CLASSPATH:antlr java Test
+   CLASSPATH=$CLASSPATH:antlr:. java Test
    if [ $? -eq 0 ] ; then echo "passed"; else echo "failed"; fi
 }
 
 echo "*** testing ddl ***"
 cat ~/tpch/dss.ddl | dotest
 
-for n in $(seq 1 22)
+for n in {1..22}
 do
    test $n -eq 15 && continue
 
    echo "*** testing query $n ***"
    < ~/tpch/queries/$n.sql tr -d '\r' | \
-   sed 's/^:x$//' | \
-   sed 's/^:o$//' | \
-   sed 's/^:n -\?[0-9]\+$//' | \
-   sed "s/\([^']\):\([0-9]\+\)/\\1\\2/g" | \
+   sed 's/^:[xo]$//' | \
+   perl -pe 's/^:n -?[0-9]+//' | \
+   perl -pe "s/([^']):([0-9]+)/\\1\\2/g" | \
    dotest
 done
